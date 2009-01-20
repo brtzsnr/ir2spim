e2c6f44e4c607f47943a996fa1c3c429fc7aff33
alexandru.mosoi <alexandru.mosoi@37d2ae08-cad6-11dd-a8b6-f5aca7b582bb>
2009-01-20 15:18:33 +0000

    updated constant-propagate-bb

--- tests/constant-propagation.py
+++ tests/constant-propagation.py
@@ -4,39 +4,39 @@ import random
 import copy
 from lib import *
 
-# registru temporar: 0
-range = (-99, +99)
+def constantPropagate(values, start, stop, steps):
+	# valorile registrilor
 
-def constantPropagate(regs, steps):
-	value = {}
-	for r in regs:
-		value[r] = None
 
-	all('# constantPropagate(%s, %s)', regs, steps)
+	# all('# constantPropagate(%s, %d, %d, %d)', values, start, stop, steps)
 	for i in xrange(steps):
-		dst = random.choice(regs)
-		if value[dst] is None or random.randint(0, 1):
+		dst = random.choice(values.keys())
+
+		if values.get(dst) is None or random.randint(0, 1):
 			# atribuim o constanta
-			value[dst] = random.randint(0, 99)
-			all('VR%d <- %d', dst, value[dst])
+			values[dst] = random.randint(start, stop)
+			all('VR%d <- %d', dst, values[dst])
 			all('VI0 <- VR%d', dst)
 			all('VR100 <- call PrintInteger')
 		else:
 			# copiem un registru
 			while True:
-				src = random.choice(regs)
-				if src == dst:
-					continue
+				src = random.choice(values.keys())
+				if src != dst:
+					break
 
-				one('VR%d <- VR%d', src, dst)
-				two('VR%d <- %d', src, value[dst])
-				all('VI0 <- VR%d', src)
-				all('VR100 <- call PrintInteger')
+			values[src] = values[dst]
+			one('VR%d <- VR%d', src, dst)
+			two('VR%d <- %d', src, values[dst])
+			all('VI0 <- VR%d', src)
+			all('VR100 <- call PrintInteger')
 
-				value[src] = value[dst]
-				break
+
+		all('')
 
 	all('')
+
+
 
 def Func1():
 	# constant propagation, BB
@@ -44,26 +44,52 @@ def Func1():
 	all('Func1:')
 	inc()
 
-	constantPropagate(xrange(20, 60), 1024)
+	regs = dict([(i, None) for i in xrange(16, 32)])
+	constantPropagate(regs, 16, 32, 200)
 
 	all('return 0')
 	all('')
 
 
+_max_depth = 1
+_values = dict([(i, None) for i in xrange(20, 60)])
+
+
+def Func2(depth=0):
+	# constant propagation + branches
+
+	constantPropagate(_values, 0, 1, random.randint(1, 1))
+	if depth == _max_depth:
+		return
+
+	for i in xrange(1):
+		while True:
+			which = random.choice(_values.keys())
+			if _values[which] is not None:
+				break
+
+		branch_then(which, _values[which])
+		Func2(depth + 1)
+		branch_else()
+		Func2(depth + 1)
+		branch_endif()
+
 
 if __name__ == '__main__':
-	iropen('constant-propagate-bb')
+	iropen('constant-propagate-branch')
 
 	all('.code')
 	library()
 
 	Func1()
 
+	# Func2()
+
 	dec()
 	all('Main:')
 	inc()
 
-	all('VR100 <- call Func1')
+	all('VR100 <- call Func2')
 	all('loadl VR100 new_line')
 	all('VI0 <- VR100')
 	all('VR100 <- call OutString')
