93d7a0d92d8db387b09740f5b357b1c700672fe0
alexandru.mosoi <alexandru.mosoi@37d2ae08-cad6-11dd-a8b6-f5aca7b582bb>
2009-01-20 16:46:45 +0000

    more tests

--- tests/constant-propagation.py
+++ tests/constant-propagation.py
@@ -31,17 +31,13 @@ def constantPropagate(values, start, stop, steps):
 			all('VI0 <- VR%d', src)
 			all('VR100 <- call PrintInteger')
 
-
 		all('')
-
-	all('')
-
 
 
 def Func1():
 	# constant propagation, BB
 	dec()
-	all('Func1:')
+	all('Func:')
 	inc()
 
 	regs = dict([(i, None) for i in xrange(16, 32)])
@@ -51,45 +47,67 @@ def Func1():
 	all('')
 
 
-_max_depth = 1
-_values = dict([(i, None) for i in xrange(20, 60)])
+_max_depth = 4
+_values = dict([(i, None) for i in xrange(16, 24)])
 
 
 def Func2(depth=0):
 	# constant propagation + branches
+	global _values
 
-	constantPropagate(_values, 0, 1, random.randint(1, 1))
+	constantPropagate(_values, 0, 1, random.randint(1, 2))
 	if depth == _max_depth:
 		return
 
-	for i in xrange(1):
+	for i in xrange(random.randint(1, 4)):
 		while True:
 			which = random.choice(_values.keys())
 			if _values[which] is not None:
 				break
 
+		constantPropagate(_values, 0, 1, random.randint(1, 2))
+
+		saved = copy.copy(_values)
 		branch_then(which, _values[which])
+
+		_values = copy.copy(saved)
 		Func2(depth + 1)
+		if saved[which]: final = copy.copy(_values)
+
 		branch_else()
+
+		_values = copy.copy(saved)
 		Func2(depth + 1)
+		if not saved[which]: final = copy.copy(_values)
+
 		branch_endif()
+		_values = copy.copy(final)
+		# all('# regs = %s', _values)
 
 
 if __name__ == '__main__':
-	iropen('constant-propagate-branch')
+	iropen('constant-propagate-branch-long')
 
 	all('.code')
 	library()
 
-	Func1()
+	# Func1()
 
-	# Func2()
+	dec()
+	all('Func:')
+	inc()
 
+	Func2()
+	all('return 0')
+	all('')
+
+
+	# Main
 	dec()
 	all('Main:')
 	inc()
 
-	all('VR100 <- call Func2')
+	all('VR100 <- call Func')
 	all('loadl VR100 new_line')
 	all('VI0 <- VR100')
 	all('VR100 <- call OutString')
