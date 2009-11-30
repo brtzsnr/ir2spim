96832edce843a55bafdf9cd0fa269fc768a52ae8
ralucav <ralucav@37d2ae08-cad6-11dd-a8b6-f5aca7b582bb>
2009-11-30 19:29:31 +0000

    Cool IR Parser

---
+++ trunk/cool/coolc.template.cfadfa/src/ro/pub/cpl/coolir/IntConstant.java
@@ -0,0 +1,17 @@
+package ro.pub.cpl.coolir;
+
+public class IntConstant extends Operand {
+	private int value;
+
+	public IntConstant(String stringValue) {
+		this.value = Integer.parseInt(stringValue);
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+	public String toString() {
+		return value + "";
+	}
+}
