1d520783a279778ecd1d02aed4e66eafb169c0e7
bogdannitulescu@gmail.com <bogdannitulescu@gmail.com@37d2ae08-cad6-11dd-a8b6-f5aca7b582bb>
2013-04-30 06:30:55 +0000

    Added basic compiler structure to project

--- trunk/sq2/SQ2-Interpreter-Hello1/src/ro/pub/cpl/sq2/hello1/Program.java
+++ trunk/sq2/SQ2-Interpreter-Hello1/src/ro/pub/cpl/sq2/hello1/Program.java
@@ -1,5 +1,6 @@
 package ro.pub.cpl.sq2.hello1;
 
+import java.io.PrintStream;
 import java.util.HashMap;
 
 public class Program {
@@ -29,5 +30,43 @@ public class Program {
 			throw new LanguageException("Function " + f.name + " defined twice");
 		
 		functions.put(f.name, f);
+	}
+
+	final String STARTUP_LLVM_CODE = 
+"@.str = private constant [3 x i8] c\"%s\\00\"\n"+
+"@.strd = private  constant [3 x i8] c\"%d\\00\"\n"+
+"\n"+
+"define void @__sq2__print(i8* %text)  {\n"+
+"  %1 = call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([3 x i8]* @.str, i32 0, i32 0), i8* %text)\n"+
+"  ret void\n"+
+"}\n"+
+"\n"+
+"define void @__sq2__write(i32 %number)  {\n"+
+"  %1 = call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([3 x i8]* @.strd, i32 0, i32 0), i32 %number)\n"+
+"  ret void\n"+
+"}\n"+
+"define i32 @__sq2__read() {\n"+
+"  %value = alloca i32\n"+
+"  store i32 0, i32* %value\n"+
+"  %1 = call i32 (i8*, ...)* @scanf(i8* getelementptr inbounds ([3 x i8]* @.strd, i32 0, i32 0), i32* %value)\n"+
+"  %2 = load i32* %value\n"+
+"  ret i32 %2\n"+
+"}\n"+
+"\n"+
+"declare i32 @printf(i8*, ...)\n"+
+"\n"+
+"declare i32 @scanf(i8*, ...)\n"+
+"\n"+
+"define i32 @main(i32 %argc, i8** %argv)  {\n"+
+"  call void @start()\n"+
+"  ret i32 0\n"+
+"}\n"+
+"\n"+
+";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n";
+
+	public void generate(PrintStream printStream) {
+		printStream.println(STARTUP_LLVM_CODE);
+		for (Function f : functions.values())
+			f.generate(printStream);
 	}
 }
