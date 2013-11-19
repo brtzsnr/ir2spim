f8d0d81bcb5609fa7e28b23742460f3a450c6915
bogdannitulescu@gmail.com <bogdannitulescu@gmail.com@37d2ae08-cad6-11dd-a8b6-f5aca7b582bb>
2013-11-19 09:12:30 +0000

    UNIX style classpath

--- trunk/lcpl/test.semant/mylcplrun.sh
+++ trunk/lcpl/test.semant/mylcplrun.sh
@@ -5,8 +5,8 @@ then
 fi
 FNAME=`basename $2`
 java -jar LCPLParser.jar $2 $FNAME.ast
-#java -cp "$1/bin:$1/lib/*" LCPLSemant $FNAME.ast $FNAME.run
-java -cp "$1/bin;$1/lib/*" LCPLSemant $FNAME.ast $FNAME.run
+java -cp "$1/bin:$1/lib/*" LCPLSemant $FNAME.ast $FNAME.run
+#java -cp "$1/bin;$1/lib/*" LCPLSemant $FNAME.ast $FNAME.run
 if [ -f $FNAME.run ]
 then
 	java -jar LCPLSemant.jar $FNAME.run --run
